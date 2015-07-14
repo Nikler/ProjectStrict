da57de15eecd58c7fc404d4ece92c806b6366641
Nikler <nikler359@gmail.com>
2015-07-14 15:09:25 +0300

    Installed all plugins ,but still not running

--- Gruntfile.js
+++ Gruntfile.js
@@ -28,11 +28,57 @@ module.exports = function(grunt) {
 					"css/style.min.css":["css/style.css"]
 				}
 			}
+		},
+		imagemin: {
+			images: {
+				options: {
+					optimizationLevel: 1
+				},
+				files: [{
+					expand: true,
+					src: ["img/**/*.{png,jpg,gif,svg}"]
+				}]
+			}
+		},
+		htmlmin: {
+			options: {
+				removeComments: true,
+				collapseWhiteSpace: true,
+				collapseBooleanAttributes: true,
+				caseSensitive: true,
+				keepClosingSlash: false
+			},
+			html: {
+				files: {
+					"index.min.html":"index.html"
+				}
+			}
+		},
+		copy: {
+			build: {
+				files: [{
+					expand: true,
+					cwd: "source",
+					src: [
+						"img/**",
+						"js/**",
+						"index.html"
+					],
+					dest: "build"
+				}]
+			}
+		},
+		clean: {
+			build: ["build"]
 		}
 	});
 	grunt.registerTask("build",[
+		"clean",
+		"copy",
 		"less",
 		"autoprefixer",
-		"cssmin"
+		"cssmin",
+		"imagemin",
+		"htmlmin"
 	]);
 };
